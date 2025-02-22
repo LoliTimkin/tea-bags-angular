336691ae9fb29b48a49d7b1c3808abc30e0725be
loli.timkin <loli.timkin@yandex.ru>
2025-02-22 16:05:45 +0700

    init commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function(event){
+    new WOW().init();
+    $('.single-item').slick({
+        arrows: true,
+        prevArrow:"<img class='a-left control-c prev slick-prev' src='../images/arrow-prev.png'>",
+        nextArrow: "<img class='a-right control-c next slick-next' src='../images/arrow-next.png'>"
+        });
+    $( "#accordion" ).accordion({
+        icons: { "header": " ui-icon-caret-1-s", "activeHeader": "ui-icon-carat-1-n" },
+        heightStyle: "content"
+    })
+
+    $('#order-form').on('submit',  e => {
+        e.preventDefault();
+
+        let name = $('#name').val();
+        let surname = $('#surname').val();
+        let phone = $('#phone').val();
+        let country = $('#country').val();
+        let postCode = $('#post-code').val();
+        let address = $('#address').val();
+
+        if(name === '' || surname === '' || postCode === '' || phone === '' || country === '' || address === '') {
+            alert('Пожалуйста, заполните все поля');
+            return;
+        }
+
+        if(!/^\d{6}$/.test(postCode)){
+            alert('Пожалуйста, введите корректный индекс (число из 6 цифр)');
+            return;
+        }
+
+        $('#order-form').hide();
+        $('#text-submit-success').show();
+    });
+});
